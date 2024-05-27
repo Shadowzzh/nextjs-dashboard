9cc905ef735b094c7387a7e0f40926038a7cb980
ziheng <shadow1746556951@gmail.com>
2024-05-27 18:15:53 +0800

    chore: ts 类型修复

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
-const config = {
-  extends: ['next/core-web-vitals', 'plugin:react-hooks/recommended'],
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'next/core-web-vitals',
+  ],
+  plugins: ['@typescript-eslint'],
   rules: {
     'react-hooks/exhaustive-deps': [
       'warn',
@@ -9,5 +15,3 @@ const config = {
     ],
   },
 };
-
-export default config;
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -75,10 +75,12 @@ export async function fetchCardData() {
       invoiceStatusPromise,
     ]);
 
-    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
-    const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
-    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
-    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
+    const numberOfInvoices = Number(data[0].rows[0]?.count ?? '0');
+    const numberOfCustomers = Number(data[1].rows[0]?.count ?? '0');
+    const totalPaidInvoices = formatCurrency(data[2].rows[0]?.paid ?? '0');
+    const totalPendingInvoices = formatCurrency(
+      data[2].rows[0]?.pending ?? '0',
+    );
 
     return {
       numberOfCustomers,
@@ -145,7 +147,7 @@ export async function fetchInvoicesPages(query: string) {
       invoices.status ILIKE ${`%${query}%`}
   `;
 
-    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    const totalPages = Math.ceil(Number(count.rows[0]?.count) / ITEMS_PER_PAGE);
     return totalPages;
   } catch (error) {
     console.error('Database Error:', error);
