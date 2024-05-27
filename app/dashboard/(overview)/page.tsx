ab7f678fc06e15df4ca6925679b810ea31a27d57
ziheng <shadow1746556951@gmail.com>
2024-05-27 19:26:37 +0800

    feat: 更改仪表盘为 shadcn ui

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,6 @@
 import CardWrapper from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import { lusitana } from '@/app/ui/fonts';
 import {
   CardsSkeleton,
   LatestInvoicesSkeleton,
@@ -12,21 +11,12 @@ import { Suspense } from 'react';
 export default async function Page() {
   return (
     <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
-      </h1>
+      <h1 className={`mb-4 text-xl md:text-2xl`}>仪表盘</h1>
+      {/* <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4"> */}
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,5 +1,10 @@
 import { fetchCardData } from '@/app/lib/data';
-import { lusitana } from '@/app/ui/fonts';
+import {
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  Card as ShadcnCard,
+} from '@/components/ui/card';
 import {
   BanknotesIcon,
   ClockIcon,
@@ -26,41 +31,34 @@ export default async function CardWrapper() {
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      />
+      <Card title="收集" value={totalPaidInvoices} type="collected" />
+      <Card title="待定" value={totalPendingInvoices} type="pending" />
+      <Card title="发票总数" value={numberOfInvoices} type="invoices" />
+      <Card title="客户总数" value={numberOfCustomers} type="customers" />
     </>
   );
 }
 
-export function Card({
-  title,
-  value,
-  type,
-}: {
+interface CardProps {
   title: string;
   value: number | string;
   type: 'invoices' | 'customers' | 'pending' | 'collected';
-}) {
+}
+export function Card(props: CardProps) {
+  const { title, value, type } = props;
   const Icon = iconMap[type];
 
   return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-      <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
-      </div>
-      <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
-      >
-        {value}
-      </p>
-    </div>
+    <ShadcnCard>
+      <CardHeader className="transition duration-300 ease-out hover:scale-110">
+        <CardDescription className="flex justify-between">
+          {title}
+          {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
+        </CardDescription>
+        <CardTitle>
+          <div className="text-2xl">{value}</div>
+        </CardTitle>
+      </CardHeader>
+    </ShadcnCard>
   );
 }
