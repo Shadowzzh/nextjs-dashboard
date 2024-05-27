3d46a774487b2b477988021eb7f32341584415bb
ziheng <shadow1746556951@gmail.com>
2024-05-27 20:35:02 +0800

    feat: 最新发票使用 shadcn

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,62 +1,61 @@
 import { fetchLatestInvoices } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
-import Image from 'next/image';
 
 export default async function LatestInvoices() {
   const latestInvoices = await fetchLatestInvoices(); // Fetch data inside the component
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Latest Invoices
+        最新的发票
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: comment in this code when you get to this point in the course */}
-
-        <div className="bg-white px-6">
+        <div className="grid bg-white p-2">
           {latestInvoices.map((invoice, i) => {
             return (
-              <div
+              <ListItem
                 key={invoice.id}
-                className={clsx(
-                  'flex flex-row items-center justify-between py-4',
-                  {
-                    'border-t': i !== 0,
-                  },
-                )}
-              >
-                <div className="flex items-center">
-                  <Image
-                    src={invoice.image_url}
-                    alt={`${invoice.name}'s profile picture`}
-                    className="mr-4 rounded-full"
-                    width={32}
-                    height={32}
-                  />
-                  <div className="min-w-0">
-                    <p className="truncate text-sm font-semibold md:text-base">
-                      {invoice.name}
-                    </p>
-                    <p className="hidden text-sm text-gray-500 sm:block">
-                      {invoice.email}
-                    </p>
-                  </div>
-                </div>
-                <p
-                  className={`${lusitana.className} truncate text-sm font-medium md:text-base`}
-                >
-                  {invoice.amount}
-                </p>
-              </div>
+                invoice={invoice}
+                className={clsx({ 'border-t': i !== 0 })}
+              />
             );
           })}
         </div>
-        <div className="flex items-center pb-2 pt-6">
+        <div className="flex items-center pb-2 pt-4">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+          <h3 className="ml-2 text-sm text-gray-500 ">刚刚更新</h3>
         </div>
       </div>
+    </div>
+  );
+}
+
+function ListItem(params: {
+  className?: string;
+  invoice: Awaited<ReturnType<typeof fetchLatestInvoices>>[0];
+}) {
+  const { invoice } = params;
+
+  return (
+    <div
+      className={clsx(
+        'flex items-center gap-4 p-4 transition duration-200 ease-out hover:z-10 hover:scale-105 hover:shadow-[0_0px_12px_2px_rgba(0,0,0,0.05)]',
+      )}
+    >
+      <Avatar className="hidden h-9 w-9 sm:flex">
+        <AvatarImage
+          src={invoice.image_url}
+          alt={`${invoice.name}'s profile picture`}
+        />
+        <AvatarFallback>OM</AvatarFallback>
+      </Avatar>
+      <div className="grid gap-1">
+        <p className="text-sm font-medium leading-none">{invoice.name}</p>
+        <p className="text-sm text-muted-foreground">{invoice.email}</p>
+      </div>
+      <div className="ml-auto font-medium">{invoice.amount}</div>
     </div>
   );
 }
